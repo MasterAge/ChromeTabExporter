fdd752f1e80a2e396d573ad2aa58c45525befde8
Adrian Seddon <sir.adrian8@gmail.com>
2022-12-24 23:53:39 +1100

    master: Added favicon and recreated original mvp

--- popup/popup.js
+++
@@ -1,8 +0,0 @@
-const tabs = await chrome.tabs.query({currentWindow: true,})
-async function exportTabs() {
-    const tabExport = tabs.map(tab => tab.url).join("\n");
-    await navigator.clipboard.writeText(tabExport);
-}
-
-document.querySelector("#exportbutton").addEventListener("click", exportTabs)
-document.querySelector("#tabCount").textContent = tabs.length.toString(10);
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+enum TabFields {
+    TITLE,
+    URL,
+    INDEX
+}
+
+interface AppState {
+    tabs: Array<chrome.tabs.Tab>;
+    delim: string;
+    fieldOrder: Array<TabFields>
+}
+
+class App extends React.Component<{}, AppState> {
+
+    constructor(props: Readonly<{}>) {
+        super(props);
+        this.state = {
+            tabs: [],
+            delim: " | ",
+            fieldOrder: [TabFields.URL]
+        }
+
+        chrome.tabs.query({currentWindow: true})
+            .then(tabList => this.setState({tabs: tabList}));
+    }
+
+    populateFields = (tab: chrome.tabs.Tab) => {
+        return this.state.fieldOrder.map(field => {
+            if (field == TabFields.URL) {
+                return tab.url;
+            } else if (field == TabFields.TITLE) {
+                return tab.title;
+            } else if (field == TabFields.INDEX) {
+                return tab.index;
+            }
+        })
+    }
+
+    exportTabs = () => {
+        const tabExport = this.state.tabs.map(tab => this.populateFields(tab).join(this.state.delim)).join("\n");
+        navigator.clipboard.writeText(tabExport).then(() => {});
+    }
+
+
+    setDelim = (event: ChangeEvent<HTMLInputElement>) => {
+        this.setState({delim: event.target.value})
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h3 className="heading">Tab Export</h3>
+                <div>
+                    <span>Tab field separator: </span><input onChange={this.setDelim} className="delimInput"/>
+                </div>
+                <br />
+                <button onClick={this.exportTabs}>Export Window's Tabs</button>
+            </div>
+        );
+    }
 }
 
 export default App;
